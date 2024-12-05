b4f9b948276ca331fe406f366f77f397309d68e3
San7o <santigio2003@gmail.com>
2024-12-05 17:53:52 +0100

    etc: fixed texlive path

--- etc/profile.d/texlive.sh
+++ etc/profile.d/texlive.sh
@@ -2,10 +2,10 @@
 TEXLIVE_PREFIX=/usr
 export TEXLIVE_PREFIX
 
-export PATH=$PATH:\$TEXLIVE_PREFIX/texmf-dist/doc/info
-export PARH=$PATH:\$TEXLIVE_PREFIX/bin/x86_64-linux
+export PATH=$PATH:$TEXLIVE_PREFIX/texmf-dist/doc/info
+export PATH=$PATH:$TEXLIVE_PREFIX/bin/x86_64-linux
 
-TEXMFCNF=\$TEXLIVE_PREFIX/texmf-dist/web2c
+TEXMFCNF=$TEXLIVE_PREFIX/texmf-dist/web2c
 export TEXMFCNF
 
 # End texlive setup
