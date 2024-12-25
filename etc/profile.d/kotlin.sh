765cd49b9307b4be4d7e39f8d31d736a1773988d
San7o <santigio2003@gmail.com>
2024-12-26 00:53:59 +0100

    lfs: updates

---
+++ etc/profile.d/kotlin.sh
@@ -0,0 +1,10 @@
+# Begin /etc/profile.d/kotlin.sh
+
+# Set JAVA_HOME directory
+JAVA_HOME=
+
+# Adjust PATH
+export PATH=$PATH:/opt/kotlinc/bin:/opt/gradle-8.12/bin:/opt/android-sdk/cmdline-tools/latest/bin:/opt/android-sdk/emulator:/opt/android-sdk/platform-tools/
+export ANDROID_HOME=/opt/android-sdk
+
+# End /etc/profile.d/kotlin.sh
